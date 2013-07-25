a540201e7b4f8a39a7d038934f9d57dbf4c518fd
Matteo Collina <matteo.collina@gmail.com>
2013-07-25 12:31:05 +0100

    Initial import.

---
+++ test/common.js
@@ -0,0 +1,30 @@
+global.sinon = require("sinon");
+global.chai = require("chai");
+global.expect = require("chai").expect;
+
+var portCounter = 30042;
+global.nextPort = function() {
+  return ++portCounter;
+};
+
+var bunyan = require("bunyan");
+
+global.globalLogger = bunyan.createLogger({
+  name: "moscaTests",
+  level: 60
+});
+
+global.moscaSettings = function() {
+  return {
+    port: nextPort(),
+    logger: {
+      name: "moscaTests",
+      level: 60
+    }
+  };
+};
+
+var sinonChai = require("sinon-chai");
+chai.use(sinonChai);
+
+global.ponte = require("../");
