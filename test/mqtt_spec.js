4e7d50fdfdf08db149ee1799a2f9213c2dca4f88
Matteo Collina <matteo.collina@gmail.com>
2013-07-26 10:14:50 +0100

    Added one more tests for MQTT retained messages.

--- test/mqtt_spec.js
+++ test/mqtt_spec.js
@@ -1,4 +1,5 @@
 
+var request = require("supertest");
 var mqtt = require("mqtt");
 var ponte = require("../");
 
@@ -25,6 +26,16 @@ describe("Ponte as an MQTT server", function() {
         expect(topic).to.eql("hello");
         expect(payload).to.eql("world");
         done();
+      });
+  });
+
+  it("should expose retained messages to HTTP", function(done) {
+    var client = mqtt.createClient(settings.mqtt.port);
+    client
+      .publish("hello", "world", { retain: true, qos: 1 }, function() {
+        request(instance.rest.http)
+          .get("/topics/hello")
+          .expect(200, "world", done);
       });
   });
 });
