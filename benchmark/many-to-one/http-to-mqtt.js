d14875a2c1306eaa10be04b7b38853a5e2828e8f
Matteo Collina <matteo.collina@gmail.com>
2013-10-25 09:23:13 +0000

    Added http-to-mqtt new results with TCP tuning.

--- benchmark/many-to-one/http-to-mqtt.js
+++ benchmark/many-to-one/http-to-mqtt.js
@@ -5,26 +5,36 @@ var mqtt = require("mqtt")
   , total = 10000
   , sent = 0
   , received = 0
+  , agent = new (require("http")).Agent()
   , listener = mqtt.createClient()
   , print = function(text) {
               process.stdout.write(text + "\n");
             }
   , start = null
-  , publish = function() {
-                  console.error("client connected, sending the message");
-                  start = Date.now();
-                  
-                  async.timesSeries(total, function(id, cb) { 
-	            setTimeout(cb, 1)
-                    request
+  , doReq = function() {
+                    var req =request
                         .put('http://localhost:3000/topics/hello')
                         .send("world")
                         .end(function(error, res){
+			  if (error) {
+setTimeout(doReq(), 10)
+console.error(error)
+}
                           //if (sent++ % (total / 10)) {
                           //  console.error("sent", sent)
                           //}
                         });
+			//req.agent(agent)
+}
+  , publish = function() {
+                  console.error("client connected, sending the message");
+                  start = Date.now();
+
+                  async.times(total, function(id, cb) { 
+setImmediate(doReq)
+setImmediate(cb)
 }, function() {});
+                  
                 }
 
 request
@@ -53,4 +63,4 @@ request
     });
   });
 
-require('http').globalAgent.maxSockets = 20000;
+agent.maxSockets = 1000;
