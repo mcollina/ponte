2611176f740162118fe2069df80b6d02942a1f0e
Matteo Collina <matteo.collina@gmail.com>
2013-12-18 10:56:47 -0800

    Added Eclipse copyright header.

--- benchmark/one-to-many/coap-to-coap.js
+++ benchmark/one-to-many/coap-to-coap.js
@@ -1,3 +1,17 @@
+/*******************************************************************************
+ * Copyright (c) 18-dec-2013 University of Bologna
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Eclipse Distribution License v1.0 which accompany this distribution.
+ *
+ * The Eclipse Public License is available at 
+ *    http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at 
+ *   http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * Contributors:
+ *    Matteo Collina - initial API and implementation and/or initial documentation
+ *******************************************************************************/
 
 var coap = require("coap")
   , total = 5000
